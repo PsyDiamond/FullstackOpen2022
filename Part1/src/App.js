367cfa8b3b440592c17d419cbbb876f527a159db
Lex Talionis <lextalionis1488@gmail.com>
2022-04-27 05:45:48 +0100

    Multi components

--- Part1/src/App.js
+++ Part1/src/App.js
@@ -1,14 +1,19 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
+  
   return (
   <div>
-  <p>Hello world, it is {now.toString()}</p>
-  <p>
-    {a} plus {b} is {a+b}
-  </p>
+  <h1>Greetings</h1>
+  <Hello />
+  <Hello />
+  <Hello />
+  <Hello />
   </div>
   )
 }
